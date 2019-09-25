81a325444da1ed3eecc2f74bcba32e18033856ef
Lisa Castaignede <castai_l@epita.fr>
2019-09-25 21:34:46 +0200

    Optimisation of method

--- src/Game/PlayerIA/CastailPlayer.php
+++ src/Game/PlayerIA/CastailPlayer.php
@@ -25,44 +25,20 @@ class CastailPlayer extends Player
       $paperCount = 0;
       $scissorsCount = 0;
       for ($i = 1; $i <= $this->result->getNbRound(); $i++) {
-        if ($this->result->getLastChoiceFor($this->opponentSide) == 'rock')
+        $lastChoice = $this->result->getLastChoiceFor($this->opponentSide);
+        if ($lastChoice == 'rock')
           $rockCount++;
-        if ($this->result->getLastChoiceFor($this->opponentSide) == 'paper')
+        if ($lastChoice == 'paper')
           $paperCount++;
-        if ($this->result->getLastChoiceFor($this->opponentSide) == 'scissors')
+        if ($lastChoice == 'scissors')
           $scissorsCount++;
         if ($rockCount > $paperCount) {
-          if ($scissorsCount > $rockCount)
+          if ($scissorsCount > $rockCount && $lastChoice != 'scissors')
             return parent::rockChoice();
           return parent::paperChoice();
         }
         return parent::scissorsChoice();
       }
-
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-       // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-        
-        return parent::paperChoice();            
+      return parent::paperChoice();            
   }
 };
