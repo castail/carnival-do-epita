23172cbe24df6c1f79887076f80d53fefe236e95
Lisa Castaignede <castai_l@epita.fr>
2019-09-25 21:42:39 +0200

    Bad strategy changed

--- src/Game/PlayerIA/CastailPlayer.php
+++ src/Game/PlayerIA/CastailPlayer.php
@@ -26,14 +26,16 @@ class CastailPlayer extends Player
       $scissorsCount = 0;
       for ($i = 1; $i <= $this->result->getNbRound(); $i++) {
         $lastChoice = $this->result->getLastChoiceFor($this->opponentSide);
+
         if ($lastChoice == 'rock')
           $rockCount++;
         if ($lastChoice == 'paper')
           $paperCount++;
         if ($lastChoice == 'scissors')
           $scissorsCount++;
+
         if ($rockCount > $paperCount) {
-          if ($scissorsCount > $rockCount && $lastChoice != 'scissors')
+          if ($scissorsCount > $rockCount)
             return parent::rockChoice();
           return parent::paperChoice();
         }
